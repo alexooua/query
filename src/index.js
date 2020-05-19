e2bc6b73ba59d8de3ceff606c96b62d135d500ed
alex <alextotalk@gmail.com>
2020-05-19 23:56:21 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 import './index.css';
-import {getImages} from './service'
+import {getImages, getTasks} from './service'
 
 const imgNumber=document.querySelector("#img-number")
 const pageNumber = document.querySelector("#page-number")
 const clickMeButton = document.querySelector('#click-me')
+const getTasksButton = document.querySelector('#get-tasks')
+
 
 clickMeButton.addEventListener(
     'click',
     ()=>{const promise = getImages(pageNumber,imgNumber)
-
-        promise.then(onDataReceived)
+        promise.then(onImgReceived)
     })
 
-function onDataReceived (arr) {
+getTasksButton.addEventListener(
+    'click',
+    ()=>{const promise = getTasks (pageNumber,imgNumber)
+        promise.then(onTasksReceived)
+    })
+
+function onImgReceived (arr) {
 
     arr.forEach(el => {
         const img = document.createElement('img')
         img.src = el.thumbnail
         document.querySelector('#result').appendChild(img)
+    })
+}
+function onTasksReceived (tasks) {
+    tasks.forEach(task => {
+        const li = document.createElement('li')
+        li.innerHTML=task.title
+        document.querySelector('#tasks-result').appendChild(li)
     })
 }
 
--- src/service.js
+++ src/service.js
@@ -15,6 +15,18 @@ export function getImages(pageNumber, imgNumber) {
         `https://repetitora.net/api/JS/Images?page=${pageNumber.value}&count=${imgNumber.value}`)
 
      return promise.then((response) => {
+         console.log(response)
+            return response.data
+        }
+    )
+}
+
+export function getTasks( ) {
+    let promise = axios.get(
+        `https://repetitora.net/api/js/Tasks?widgetId=213131`)
+
+    return promise.then((response) => {
+            console.log(response)
             return response.data
         }
     )
